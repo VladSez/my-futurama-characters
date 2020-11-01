b465481ef1a989abeab4e4949409ff4eae8f9e5f
Vlad <smallredeagle@gmail.com>
2020-11-02 00:21:38 +0300

    Experiments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
-import './App.css';
-import Fuse from 'fuse.js';
+import React, { useState } from "react";
+import "./App.css";
+import Fuse from "fuse.js";
+import Highlight from "react-highlighter";
 
-import characters from './characters.json';
+import characters from "./characters.json";
+
+const options = {
+  shouldSort: true,
+  includeMatches: true,
+  threshold: 0.3,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  findAllMatches: true,
+  keys: ["name", "company", "species"]
+};
 
 function App() {
-  const [query, updateQuery] = useState('');
+  const [query, updateQuery] = useState("");
 
-  const fuse = new Fuse(characters, {
-    keys: [
-      'name',
-      'company',
-      'species'
-    ],
-    includeScore: true
-  });
+  const fuse = new Fuse(characters, options);
 
   const results = fuse.search(query);
-  const characterResults = query ? results.map(character => character.item) : characters;
+  const characterResults = query
+    ? results.map((character) => character.item)
+    : characters;
 
   function onSearch({ currentTarget }) {
     updateQuery(currentTarget.value);
@@ -25,7 +33,6 @@ function App() {
 
   return (
     <>
-
       <header className="App-header">
         <div className="container">
           <h1>Futurama Characters</h1>
@@ -34,26 +41,37 @@ function App() {
 
       <main className="container">
         <ul className="characters">
-          {characterResults.map(character => {
+          {characterResults.map((character) => {
             const { name, company, species, thumb } = character;
             return (
-              <li key={name} className="character">
-                <span className="character-thumb" style={{
-                  backgroundImage: `url(${thumb})`
-                }} />
+              <li key={name + 1} className="character">
+                {/* <Highlight search={query} style={{ fontWeight: "bold" }}>
+                  {name}
+                </Highlight> */}
+
+                <span
+                  className="character-thumb"
+                  style={{
+                    backgroundImage: `url(${thumb})`
+                  }}
+                />
                 <ul className="character-meta">
                   <li>
-                    <strong>Name:</strong> { name }
+                    <Highlight search={query} style={{ fontWeight: "bold" }}>
+                      {name}
+                    </Highlight>
                   </li>
                   <li>
-                    <strong>Company:</strong> { company }
+                    <Highlight search={query} style={{ fontWeight: "bold" }}>
+                      {company}
+                    </Highlight>
                   </li>
                   <li>
-                    <strong>Species:</strong> { species }
+                    <strong>Species:</strong> {species}
                   </li>
                 </ul>
               </li>
-            )
+            );
           })}
         </ul>
         <aside>
@@ -67,7 +85,8 @@ function App() {
       <footer>
         <div className="container">
           <p>
-            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a> via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
+            Images from <a href="http://www.cc.com/shows/futurama">Futurama</a>{" "}
+            via <a href="https://futurama.fandom.com/">futurama.fandom.com</a>
           </p>
         </div>
       </footer>
